961ee26fab270b15b29844861b0866f80b289358
CoolLikeGoose <niken54@mail.ru>
2020-06-16 13:47:37 +0300

    Added simple movement

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private bool lockFPS;
+
+    private void Awake()
+    {
+        if (lockFPS) { Application.targetFrameRate = 60; }
+        else { Application.targetFrameRate = -1; }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 800f;
+    [SerializeField] private float junpForce;
+
+    private bool jumpAbility = false;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpAbility)
+        {
+            jumpAbility = false;
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector2(0, junpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime , rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        jumpAbility = true;
+    }
+}
