6a78820dc640c4ad902faf309892e6e4af9460b4
CoolLikeGoose <niken54@mail.ru>
2020-06-17 17:03:04 +0300

    Added gun aim animation

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+
+        if (angle > 90 || angle < -90) { transform.localScale = new Vector3(1, -1, 1); }
+        else if (angle < 90 && angle > 0 || angle > -90 && angle < 0) { transform.localScale = new Vector3(1, 1, 1); }
+
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +10,15 @@ public class PlayerController : MonoBehaviour
     private bool jumpAbility = false;
     private int jumpsLeft = 1;
 
+    private Transform gun;
+ 
     private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        gun = transform.GetChild(0);
     }
 
     private void Update()
@@ -26,6 +31,8 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(new Vector2(0, junpForce), ForceMode2D.Impulse);
         }
+
+        GunLookAtCursor();
     }
 
     private void LateUpdate()
@@ -37,5 +44,24 @@ public class PlayerController : MonoBehaviour
     {
         jumpAbility = true;
         jumpsLeft = jumpsAvaible;
+    }
+
+    private void GunLookAtCursor()
+    {
+        /*
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        lookPos = lookPos - transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        gun.position = Quaternion.AngleAxis(angle, Vector3.forward);
+        */
+        /*
+        Vector3 mouseInWorld = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gun.right = mouseInWorld - transform.position;
+        
+        Vector3 mouseInWorld = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseInWorld.Normalize();
+        gun.position = mouseInWorld - transform.position;
+        gun.right = mouseInWorld - transform.position;
+        */
     }
 }
